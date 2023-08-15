affa4c73c75ac9b17e508658ed38c9e3d1b05437
Victor Alexandre <victoralexandre29051986@gmail.com>
2023-08-15 10:57:09 -0300

    Finalizado

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from pytesseract import pytesseract
 from PIL import Image
 import os
 
+#Não é necessário usar essa biblioteca
 img = Image.open('imagem2.jpg')
 
 # pytesseract.tesseract_cmd=r'C:\tesseract\tesseract.exe'
 pytesseract.tesseract_cmd = os.path.join("C:\\tesseract","tesseract.exe")
 
-
+#duas maneiras que pode ser feito, mas o primeiro tem que usar a biblioteca PIL
+#text = pytesseract.image_to_string(img)
 text = pytesseract.image_to_string(f'imagem3.jpg')
 
 linhas = text.split('\n')
